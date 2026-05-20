e3bd7d690fe4cdafd5a448ad51662d133483506b
rramigl316 <rramigl316@g.educaand.es>
2026-05-20 19:21:21 +0200

    footer responsive

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,36 +7,51 @@ import tiktok from "../../assets/images/tiktok.svg";
 
 function Footer() {
   return (
-    <footer className="flex justify-between items-start p-5 w-full box-border shadow-[0px_-4px_6px_rgba(0,0,0,0.1)] mt-7">
-
+    <footer
+      className="
+        flex flex-col sm:flex-row
+        justify-between
+        items-start
+        gap-6 sm:gap-0
+        p-5
+        w-full
+        box-border
+        shadow-[0px_-4px_6px_rgba(0,0,0,0.1)]
+        mt-7
+      "
+    >
       {/* COLUMNAS */}
-      <div className="flex flex-col gap-2.5">
+      <div className="flex flex-col gap-2.5 text-sm sm:text-base">
         <p>Sobre nosotros</p>
         <p>Contactos</p>
       </div>
 
-      <div className="flex flex-col gap-2.5">
+      <div className="flex flex-col gap-2.5 text-sm sm:text-base">
         <p>Preguntas frecuentes</p>
         <p>Política de envíos</p>
       </div>
 
-      <div className="flex flex-col gap-2.5">
+      <div className="flex flex-col gap-2.5 text-sm sm:text-base">
         <p>Información legal</p>
         <p>Suscripciones</p>
       </div>
 
       {/* ICONOS */}
-      <div className="flex items-center gap-3.5">
-
-        <img src={instagram} alt="instagram" />
-        <img src={twitter} alt="twitter" />
-        <img src={facebook} alt="facebook" />
-        <img src={youtube} alt="youtube" />
-        <img src={pinterest} alt="pinterest" />
-        <img src={tiktok} alt="tiktok" />
-
+      <div
+        className="
+          flex flex-wrap sm:flex-nowrap
+          items-center
+          gap-3
+          mt-2 sm:mt-0
+        "
+      >
+        <img src={instagram} alt="instagram" className="h-5 sm:h-6" />
+        <img src={twitter} alt="twitter" className="h-5 sm:h-6" />
+        <img src={facebook} alt="facebook" className="h-5 sm:h-6" />
+        <img src={youtube} alt="youtube" className="h-5 sm:h-6" />
+        <img src={pinterest} alt="pinterest" className="h-5 sm:h-6" />
+        <img src={tiktok} alt="tiktok" className="h-5 sm:h-6" />
       </div>
-
     </footer>
   );
 }
